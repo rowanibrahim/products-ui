87086d0c02ff7d3a159db798724cae0e51e1808d
rowanibrahim <rowan.ibrahim.mohsen@gmail.com>
2022-03-14 23:41:49 +0200

    add table of content

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+declare interface TableData {
+    headerRow: string[];
+    dataRows: string[][];
+}
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+
+export class ProductListComponent implements OnInit{
+    public tableData: TableData = {
+      headerRow: [ 'ID', 'Name', 'Country', 'City', 'Salary'],
+      dataRows: [
+          ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
+          ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
+          ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
+          ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
+          ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
+          ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
+      ]
+  };
+    ngOnInit(){}
+}
